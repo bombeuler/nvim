6400e7f7e70030e0082acb571556194543547cb5
Bombeuler <bombeuler@foxmail.com>
2024-06-19 15:30:02 +0800

    Init neovim config

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Some basic configure of my neovim
+local opt = vim.opt
+opt.wrap = true
---
+++ lua/plugins/fortran.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "findent",
+        "fortls",
+        "fprettify",
+      },
+    },
+  },
+}
