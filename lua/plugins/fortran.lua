07b6ec9096b29bcbcd86d87189c6a5176cf4296f
Bombeuler <bombeuler@foxmail.com>
2024-06-19 16:31:23 +0800

    Update fortran lsp and formatter

--- lua/plugins/fortran.lua
+++ lua/plugins/fortran.lua
@@ -9,4 +9,27 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "fortran" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      server = {
+        fortls = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["fortran"] = { "fprettify" },
+      },
+    },
+  },
 }
